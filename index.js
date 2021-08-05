d7bea46025a3b42915caaaf9f0f76628b790cc03
Uduak Essien <acushla4real@gmail.com>
2021-08-05 12:14:28 +0100

    Add notifyChannel method to handle github webhook

--- index.js
+++ index.js
@@ -60,6 +60,48 @@ schedule.scheduleJob('5 0 20 * * *', scheduleMessage);
 // eslint-disable-next-line no-unused-vars
 app.post('/github', (req, res) => {
 
+	async function NotifyChannel() {
+		const actionType = req.headers['x-github-event'];
+		const resbody = req.body;
+		const sender = resbody.sender;
+		const guild = client.guilds.cache.get(guiddId);
+		const channel = guild.channels.cache.get(channelId);
+
+		switch (actionType) {
+		case 'issue_comment':
+			const msg = discordMsg(sender, resbody.action, 'comment on issue', resbody.issue.url, resbody.issue.title, resbody.comment.body);
+			channel.send(msg);
+			break;
+		case 'issues':
+			const issueMsg = discordMsg(sender, resbody.action, 'an issue', resbody.issue.url, resbody.issue.title, resbody.issue.body);
+
+			channel.send(issueMsg);
+			break;
+		case 'pull_request':
+			const prMsg = discordMsg(sender, resbody.action, 'a Pull Request', resbody.pull_request.url, resbody.pull_request.title, resbody.pull_request.body);
+
+			channel.send(prMsg);
+			break;
+		case 'push':
+			const pushMsg = discordMsg(sender, 'made', 'a push', resbody.head_commit.url);
+
+			channel.send(pushMsg);
+			break;
+		case 'pull_request_review_comment':
+			const prComment = discordMsg(sender, 'commented', 'on a PR', resbody.comment.url, '', resbody.comment.body);
+
+			channel.send(prComment);
+			break;
+		case 'pull_request_review':
+			const prRev = discordMsg(sender, resbody.action, 'a PR review', resbody.html_url, '', resbody.review.body);
+
+			channel.send(prRev);
+			break;
+		default:
+			break;
+		}
+	}
+	NotifyChannel();
 });
 
 app.listen(port, () =>
